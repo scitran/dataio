a8c609cb4dc9d09fcab3d4b81c0ec0998a1d7d67
arokem <arokem@gmail.com>
2014-12-01 12:09:00 -0800

    BF: Make it install-able again.

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@
 XXX
 
 
+Dependencies:
+-dcmstack
+-pymongo
+-bson
 """
 
 from distutils.core import setup
 
 setup(name='nimsdata',
-      packages = ['nimsdata', 'nimsdata.pfile'],
+      packages = ['nimsdata', 'nimsdata.pfile', 'nimsdata.medimg'],
       package_dir = {'nimsdata':'.', 'nimsdata.pfile': './pfile'},
-      package_data={'nimsdata': ['./*.py'],
-                    'nimsdata.pfile':[ './pfile/*.py']})
+      package_data={'nimsdata': ['./*.py', './*.json'],
+                    'nimsdata.pfile':[ './pfile/*.py'],
+                    'nimsdata.medimg': ['./medimg/*.py']})
