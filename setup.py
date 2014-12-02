84e9c74af9c687cfd2c83699c2c124a9add5a27f
arokem <arokem@gmail.com>
2014-12-01 16:56:40 -0800

    Go deeper into the API tree.

--- setup.py
+++ setup.py
@@ -12,8 +12,12 @@ Dependencies:
 from distutils.core import setup
 
 setup(name='nimsdata',
-      packages = ['nimsdata', 'nimsdata.pfile', 'nimsdata.medimg'],
+      packages = ['nimsdata', 'nimsdata.pfile', 'nimsdata.medimg',
+                  'nimsdata.medimg.dcm','nimsdata.medimg.dcm.mr'],
       package_dir = {'nimsdata':'.', 'nimsdata.pfile': './pfile'},
       package_data={'nimsdata': ['./*.py', './*.json'],
                     'nimsdata.pfile':[ './pfile/*.py'],
-                    'nimsdata.medimg': ['./medimg/*.py']})
+                    'nimsdata.medimg': ['./medimg/*.py'],
+                    'nimsdata.medimg.dcm':['./medimg/dcm/*.py'],
+                    'nimsdata.medimg.dcm.mr':['./medimg/dcm/mr/*.py']
+          })
